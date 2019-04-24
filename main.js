9c0fa7cfabf41437550382bf273846069437c162
Frari <francescoriccardi86@gmail.com>
2019-04-24 18:48:16 +0200

    fine

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// dichiaro il ciclo for
 for(i=1 ;i<=100; i++){
+// numeri multipli di 5 e 3
   if(i % 3 == 0 && i % 5 == 0){
     console.log('fizzbuzz');
-
-  }else if(i % 3 == 0 && i % 5 != 0){
+    document.getElementById('list').innerHTML += '<br>fizzbuzz';
+// numeri multipli di 3
+  }else if(i % 3 == 0 ){
    console.log('fizz');
-
- }else if(i % 5 == 0 && i % 3 != 0){
+   document.getElementById('list').innerHTML += '<br>fizz';
+// numeri multipli di 5
+  }else if(i % 5 == 0 ){
    console.log('buzz');
-
+   document.getElementById('list').innerHTML += '<br>buzz';
+// numeri non multipli
   }else{
    console.log(i);
+   document.getElementById('list').innerHTML += '<br>'+ i ;
   }
 }
